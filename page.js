6763ce5ab10b8b17af140a1a8d5e41bb3fd96381
Bailee Underwood <underwbm@clarkson.edu>
2022-11-17 11:17:59 -0500

    Updates

--- page.js
+++ page.js
@@ -1,4 +1,9 @@
-function open() {
-//    document.getElementById("navbar-vertical").style.display = "block";
-//    alert("hey");
+function opens() {
+    var dropdown = document.getElementById("navbar-vertical");
+    if(dropdown.style.display == "none"){
+        dropdown.style.display="block";
+    }
+    else if(dropdown.style.display == "block"){
+        dropdown.style.display="none";
+    }
 }
